df9b55c2cf86df6da6f32e8a2afec6cda19f50eb
Ando Nary <ando.nary@gmail.com>
2022-03-31 21:06:01 +0200

    remise en bouche

--- add.test.ts
+++ add.test.ts
@@ -2,6 +2,16 @@ import { Add } from "./add";
 
 describe("Add string calculator", () => {
   it("should add my numbers", () => {
-    expect(true).toEqual(false);
+    expect(Add("")).toEqual(0);
+    expect(Add("1")).toEqual(1);
+    expect(Add("1,0")).toEqual(1);
+    expect(Add("1,1")).toEqual(2);
+    expect(Add("1,1,1")).toEqual(3);
+    expect(Add("1,1\n1")).toEqual(3);
+    expect(Add("1,1\n1\n1")).toEqual(4);
+    expect(() => Add("1,1\n1\n1,")).toThrow();
+    expect(Add("//;\n1;1;1\n1")).toEqual(4);
+    expect(Add("//sep\n1sep1sep1\n1")).toEqual(4);
+    expect(() => Add("1,-1")).toThrow();
   });
 });
--- add.ts
+++ add.ts
@@ -1,3 +1,23 @@
-export function Add() {
-  return void 0;
+export function Add(nbs: string): number {
+  let result = 0;
+  let separator = ",";
+  let newLine = "\n";
+  let newDelimiter = "//";
+  if (nbs) {
+    let lines = nbs.split(newLine);
+    for (let l = 0; lines[l]; l++) {
+      if (lines[l].startsWith(newDelimiter)) {
+        separator = lines[l].slice(newDelimiter.length);
+        continue;
+      }
+      if (lines[l].endsWith(separator)) throw new Error();
+      let line = lines[l].split(separator);
+      for (let n = 0; line[n]; n++) {
+        let value = Number(line[n]);
+        if (value < 0) throw new Error();
+        result += value;
+      }
+    }
+  }
+  return result;
 }
