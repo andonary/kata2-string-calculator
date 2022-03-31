e7751c6476d89a042038a2a6bac9dea2bda3bfd9
Ando Nary <ando.nary@gmail.com>
2022-03-31 20:41:56 +0200

    start

---
+++ add.test.ts
@@ -0,0 +1,7 @@
+import { Add } from "./add";
+
+describe("Add string calculator", () => {
+  it("should add my numbers", () => {
+    expect(true).toEqual(false);
+  });
+});
---
+++ add.ts
@@ -0,0 +1,3 @@
+export function Add() {
+  return void 0;
+}
